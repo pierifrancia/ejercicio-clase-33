7fb0fc0c9e95212324d4fe4c4cf94a08b0769e06
pierifrancia  <pieri.francia@gmail.com>
2019-10-23 22:57:40 -0300

    agregue mensajes de error para campos vacios

--- ejercicio33.js
+++ ejercicio33.js
@@ -2,11 +2,28 @@ let noviembre = "noviembre";
 let diciembre = "diciembre";
 
 const nombre = prompt ("Por favor ingresa tu nombre");
-let anio = prompt ("Ahora ingresa tu año de nacimiento");
+
+nombre == "" && alert ("No ingresaste tu nombre");
+
+nombre == "" && prompt ("Por favor ingresa tu nombre");
+
+var anio = prompt ("Ahora ingresa tu año de nacimiento");
+
+(anio == 0) && alert ("No ingresaste tu año de nacimiento");
+
+anio = prompt ("Ahora ingresa tu año de nacimiento");
+
 const mes = prompt ("Por ultimo ingresa tu mes de nacimiento");
+
+mes == "" && alert ("No ingresaste tu mes de nacimiento");
+
+mes == "" && prompt ("Por ultimo ingresa tu mes de nacimiento");
 
 Number(anio);
 
-(mes != noviembre && mes != diciembre) && alert ("Tu edad es " + (2019-anio) + " años");
+(mes != noviembre && mes != diciembre && anio != 0) && alert ("Tu edad es " + (2019-anio) + " años");
 
 (mes == noviembre || mes == diciembre) && alert ("Tu edad es " + (2019-anio-1) + " años");
+
+
+
