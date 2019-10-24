b65cf4e86e93c9af78720307a1713b584388269f
pierifrancia  <pieri.francia@gmail.com>
2019-10-23 23:50:23 -0300

    correcion de campos vacios pero no funciona del todo bien todavia

--- ejercicio33.js
+++ ejercicio33.js
@@ -11,19 +11,19 @@ var anio = prompt ("Ahora ingresa tu año de nacimiento");
 
 (anio == 0) && alert ("No ingresaste tu año de nacimiento");
 
-anio = prompt ("Ahora ingresa tu año de nacimiento");
+(anio == 0) && prompt ("Ahora ingresa tu año de nacimiento");
 
 const mes = prompt ("Por ultimo ingresa tu mes de nacimiento");
 
-mes == "" && alert ("No ingresaste tu mes de nacimiento");
+(mes == "") && alert ("No ingresaste tu mes de nacimiento");
 
-mes == "" && prompt ("Por ultimo ingresa tu mes de nacimiento");
+(mes == "") && prompt ("Por ultimo ingresa tu mes de nacimiento");
 
 Number(anio);
 
 (mes != noviembre && mes != diciembre && anio != 0) && alert ("Tu edad es " + (2019-anio) + " años");
 
-(mes == noviembre || mes == diciembre) && alert ("Tu edad es " + (2019-anio-1) + " años");
+((mes == noviembre || mes == diciembre) && anio != 0) && alert ("Tu edad es " + (2019-anio-1) + " años");
 
 
 
